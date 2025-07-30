b73283d0fa99dd04912f5642cc0fafc264d9d83d
04042015 <m.iggopramulia14@gmail.com>
2025-07-30 19:10:27 +0700

    Update postcss.config.mjs

--- langsadev/postcss.config.mjs
+++ langsadev/postcss.config.mjs
@@ -1,8 +1,6 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-const config = {
-  plugins: [tailwindcss, autoprefixer],
-};
-
-export default config;
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
