dbcb168e714e3f0df73ee1754067ae3211aaf014
04042015 <m.iggopramulia14@gmail.com>
2025-07-30 19:02:23 +0700

    Update postcss.config.mjs

--- langsadev/postcss.config.mjs
+++ langsadev/postcss.config.mjs
@@ -1,7 +1,6 @@
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
-/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: [tailwindcss, autoprefixer],
 };
